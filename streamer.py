d4ab4d01ad6fe17d9a33493e075276ba83d05cd1
Fin <spamsuckersunited@gmail.com>
2019-01-04 17:00:24 -0700

    Add warning about minimum images to use

--- streamer.py
+++ streamer.py
@@ -7,12 +7,15 @@ import os
 import re
 import io
 import base64
+from math import ceil
 
 def main():
     parser = argparse.ArgumentParser(description='Split a file into multiple streamed QR codes using a'
                                                  ' fountain coding scheme')
 
     parser.add_argument('-b', '--block-size', type=int, default=512)
+    parser.add_argument('-d', '--duration', type=int, default=300,
+                        help='The duration of each individual frame in the QR gif')
     parser.add_argument('infile', nargs='?', type=argparse.FileType('rb'),
                         default=sys.stdin.buffer, help='The filename or path of file to stream.')
 
@@ -31,7 +34,7 @@ def main():
     images = []
 
     for i, block in enumerate(data):
-        print('Generating QR code #%s' % i)
+        print('Generating QR code #%s' % (i + 1))
         qr = qrcode.QRCode(version=None,
                            box_size=15,
                            border=10)
@@ -48,7 +51,10 @@ def main():
 
     images = [j.convert('RGBA') for j in images]
 
-    images[0].save(os.path.join(current_path, timestamp, re.sub(r'\W+', '', args.infile.name) + '.gif'), format='GIF', save_all=True, append_images=images[1:], duration=300, loop=0)
+    images[0].save(os.path.join(current_path, timestamp, re.sub(r'\W+', '', args.infile.name) + '.gif'), format='GIF', save_all=True, append_images=images[1:], duration=args.duration, loop=0)
+    print('\nResults:\n-------------------------------')
+    print('Generated %s images.' % len(images))
+    print('Reccomended minimum number of images to print/display/use: %0.0f' % ceil(score * 1.2))
 
 if __name__ == '__main__':
     main()
