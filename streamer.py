8cdbbe5b40faf487ce8dfe0034032c2ef8c97ad1
Fin <spamsuckersunited@gmail.com>
2019-01-10 08:46:36 -0700

    Add changeable error correction

--- streamer.py
+++ streamer.py
@@ -8,6 +8,7 @@ import re
 import io
 import base64
 from math import ceil, floor
+from qrcode.constants import *
 
 def main():
     parser = argparse.ArgumentParser(description='Split a file into multiple streamed QR codes using  '
@@ -15,11 +16,16 @@ def main():
 
     parser.add_argument('-b', '--block-size', type=int, default=512)
     parser.add_argument('-d', '--duration', type=int, default=300,
-                        help='The duration of each individual frame in the QR gif')
+                        help='The duration of each individual frame in the QR gif. '
+                             'Default is 300 ms')
     parser.add_argument('-e', '--extra', type=int, default=8,
-                        help='The number of extra QR codes to generate')
+                        help='The number of extra QR codes to generate '
+                             'Default is 8.')
+    parser.add_argument('-r', '--error', type=int, choices=[0, 1, 2, 3], default=1,
+                        help='The level of error correction. Default is 1')
     parser.add_argument('infile', nargs='?', type=argparse.FileType('rb'),
-                        default=sys.stdin.buffer, help='The filename or path of file to stream.')
+                        default=sys.stdin.buffer, help='The filename or path of file to stream. '
+                                                       'Default is stdin.')
 
     args = parser.parse_args()
 
@@ -31,9 +37,8 @@ def main():
         try:
             data, score = lterrorcorrection.optimal_encoding(io.BytesIO(input_data), args.block_size, extra=floor(running_extra))
             break
-        except:
+        except Exception:
             running_extra += 0.25
-
 
     current_path = os.getcwd()
 
@@ -46,6 +51,12 @@ def main():
     for i, block in enumerate(data):
         print('Generating QR code #%s' % (i + 1))
         qr = qrcode.QRCode(version=None,
+                           error_correction={
+                               0: ERROR_CORRECT_L,
+                               1: ERROR_CORRECT_M,
+                               2: ERROR_CORRECT_Q,
+                               3: ERROR_CORRECT_H,
+                           }.get(args.error, 1),
                            box_size=8,
                            border=10)
         qr.add_data(base64.b64encode(block))
@@ -55,13 +66,13 @@ def main():
         images.append(img)
         img.save(os.path.join(current_path, timestamp, '%s_%s.png' % (i, timestamp)))
 
-    with open(os.path.join(current_path, timestamp, re.sub(r'\W+', '', args.infile.name)), 'wb') as f:
+    with open(os.path.join(current_path, timestamp, re.sub(r'[^\w.]+', '', args.infile.name)), 'wb') as f:
         f.write(input_data)
     args.infile.close()
 
     images = [j.convert('RGBA') for j in images]
 
-    images[0].save(os.path.join(current_path, timestamp, re.sub(r'\W+', '', args.infile.name) + '.gif'), format='GIF', save_all=True, append_images=images[1:], duration=args.duration, loop=0)
+    images[0].save(os.path.join(current_path, timestamp, re.sub(r'[^\w.]+', '', args.infile.name) + '.gif'), format='GIF', save_all=True, append_images=images[1:], duration=args.duration, loop=0)
     print('\nResults:\n-------------------------------')
     print('Generated %s images.' % len(images))
     print('Recommended minimum number of images to print/display/use: %0.0f' % ceil(score + 5))
